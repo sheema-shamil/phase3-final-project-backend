71e14fd246fd6bd3d41e2c62b930baebeb915c62
sheema <sheemashamilfarook@gmail.com>
2023-03-06 00:35:01 +0300

    completed

--- Sinatra/app /Models/server.rb
+++ Sinatra/app /Models/server.rb
@@ -3,3 +3,9 @@ require 'sinatra'
 ## Model/Dataset
 
 posts = [{title: "First Post", body: "content of first post"}]
+
+## Index route
+get '/posts' do
+    # Return all the posts as JSON
+    return posts.to_json
+end
